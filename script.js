55568d6341fe7da67c703d7b3b8401b82cd8ede9
Mary O'Hara <396579@LG203MAC14.local>
2024-03-04 14:38:00 +0000

    mary added html css and javascript

---
+++ script.js
@@ -0,0 +1,15 @@
+
+var x = prompt("what is the answer?")
+// this variable contains the answer of the user
+if(x === "42"){
+alert("you are correct")
+
+}else{
+  //this else statement covers the responses that are not meeting the condition of 42
+ var y = alert ("not correct")
+
+  //this if statement is the next prompt to determine the direction of the game or story. 
+
+
+ }
+}
